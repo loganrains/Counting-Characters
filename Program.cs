33c852656462acd122da73e5269b039cb77d77bc
loganrains <logankrains@gmail.com>
2024-05-14 15:17:37 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Diagnostics.Contracts;
+string readText = File.ReadAllText("./String.txt").ToUpper();
+
+Dictionary<char, int> counterDictionary = [];
+foreach (char letter in readText)
+{
+    if (Char.IsLetter(letter))
+    {
+        if (counterDictionary.ContainsKey(letter))
+    {
+        counterDictionary[letter]++;
+    }
+    else
+    {
+        counterDictionary.Add(letter, 1);
+    };
+    };
+};
+
+foreach (KeyValuePair<char, int> obj in counterDictionary)
+{
+    Console.WriteLine(obj.Key + ", " + obj.Value);
+};
